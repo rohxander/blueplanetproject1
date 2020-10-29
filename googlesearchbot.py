4e69903c7ebd3c0db52714d9c4b23f2d022dbbee
rohxander <rohitmanjappallil@gmail.com>
2020-10-29 21:53:57 +0530

    linked CSV and google search code

---
+++ googlesearchbot.py
@@ -0,0 +1,15 @@
+import requests, sys, webbrowser, bs4
+
+filename = "doctorlist.csv"
+headers = "Name,Type,Gender,Address1,City,PINCODE,PhoneNumber\n"
+f = open(filename,"w")
+f.write(headers)
+f.close()
+# res = request.get('https://google.com/search?q='+''.join(sys.argv[1:]))
+# res.raise_for_status()
+#
+# soup = bs4.BeautifulSoup(res.text, "html.parser")
+#
+# linkElements = soup.select('.r a')
+# linkToOpen = min(5, len(linkElements))
+# for i in range(linkToOpen):
