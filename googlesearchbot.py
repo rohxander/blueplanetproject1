a6145426690df5f22d6fe2f2a8dc824af8f11656
rohxander <rohitmanjappallil@gmail.com>
2020-10-31 23:53:10 +0530

    csv convert

--- googlesearchbot.py
+++ googlesearchbot.py
@@ -1,10 +1,47 @@
-import requests, sys, webbrowser, bs4
+# importing pandas as pd
+import pandas as pd
 
-filename = "doctorlist.csv"
-headers = "Name,Type,Gendner,Address1,City,PINCODE,PhoneNumber\n"
-f = open(filename,"w")
-f.write(headers)
-f.close()
+# read an excel file and convert
+# into a dataframe object
+df = pd.DataFrame(pd.read_excel("sample_record_1.xlsx"))
+
+# show the dataframe
+print(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import requests, sys, webbrowser, bs4
+# import pandas as pd
+# import numpy
+#
+# f = pd.read_csv("uszips.csv")
+# f = f.drop('lat',axis=2)
+# coloumns_to_keep = ['zip', 'city', 'state_name']
+# new_f = f[coloumns_to_keep]
 # res = request.get('https://google.com/search?q='+''.join(sys.argv[1:]))
 # res.raise_for_status()
 #
@@ -13,4 +50,4 @@ f.close()
 # linkElements = soup.select('.r a')
 # linkToOpen = min(5, len(linkElements))
 # for i in range(linkToOpen):
-# ffffffffffff
+# ffffff
---
+++ practice_data_handling.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy
+
+s = pd.DataFrame(
+    {
+        "Coloumns":[1,2,3,4,5],
+        "rows":[1,2,3,4,5]
+
+    }
+)
+print(s)
+print(s.columns)
